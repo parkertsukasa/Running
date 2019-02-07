aba1d29eab76991f02eff22c493af81b7ca9aa7d
TsukasaSumizono <freddiesumiz.3@gmail.com>
2019-02-07 17:26:41 +0900

    BackUp for start merge

---
+++ Assets/Parker/Scropts/InputSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSwitcher : MonoBehaviour 
+{
+
+  private BbaControl bbaControl;
+
+	// Use this for initialization
+	void Start () 
+  {
+		bbaControl = GetComponent<BbaControl> ();
+	}
+	
+
+#if UNITY_IOS || UNITY_ANDROID
+  // Update is called once per frame
+	void Update () 
+  {
+    
+	}
+#else
+	// Update is called once per frame
+	void Update () 
+  {
+		bbaControl.InputAccel = Input.GetAxis ("Vertical");
+    bbaControl.InputHundle = Input.GetAxis ("Horizontal");
+    Debug.Log (Input.GetAxis ("Horizontal"));
+	}
+#endif
+
+}
---
+++ Assets/Scripts/ParticleCtrl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ParticleCtrl : MonoBehaviour
+{
+    GameObject pObject;
+    ParticleSystem particle;
+
+    public float slowdownFactor = 0.25f;
+	public float slowdownLength = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        pObject = GameObject.Find ("Sparks");
+        particle = pObject.GetComponent<ParticleSystem> ();
+    }
+
+    void Update()
+    {
+        //パーティクルの生成を停止
+        if (Input.GetKey(KeyCode.A)){
+            particle.Stop();
+        }
+
+        //パーティクルの生成スピードを遅くする（演出系で使えそう）
+        if (Input.GetKey (KeyCode.Space)) {
+			Time.timeScale = slowdownFactor;
+			Time.fixedDeltaTime = Time.timeScale * 0.02f;
+		} else {
+			Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
+			Time.timeScale = Mathf.Clamp (Time.timeScale, 0f, 1f);
+		}
+    }
+}
